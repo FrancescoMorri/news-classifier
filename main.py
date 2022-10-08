c67827b0e211409384f4330811b010d6664df8d4
Francesco <francesco98m@gmail.com>
2022-10-08 22:41:00 +0200

    first commit, no bert

---
+++ main.py
@@ -0,0 +1,133 @@
+import numpy as np
+from torch.utils.data import TensorDataset, DataLoader, SequentialSampler
+from helpers import *
+from scraping import *
+from datetime import datetime
+import streamlit as st
+import pandas as pd
+import json
+from google.cloud import firestore
+from google.oauth2 import service_account
+
+
+def load_data_on_cloud(date, evaluation, key):
+    db = firestore.Client(credentials=key, project="news-classifier-bb21e")
+
+    doc_ref = db.collection("news-history").document("date-points")
+
+    doc_ref.update({u'points': firestore.ArrayUnion([evaluation])})
+    doc_ref.update({u'date': firestore.ArrayUnion([date])})
+
+def download_data_from_cloud(key):
+    db = firestore.Client(credentials=key, project="news-classifier-bb21e")
+
+    doc_ref = db.collection("news-history").document("date-points")
+
+    return doc_ref.get().to_dict()
+
+
+
+@st.cache
+def load_model(model_path):
+    model = BertClassifier()
+    model.load_state_dict(torch.load(model_path,map_location=torch.device('cpu')))
+    return model
+
+
+
+
+st.set_page_config("news-classifier", page_icon="💲")
+
+full_today_news = []
+key_dict = json.loads(st.secrets["textkey"])
+creds = service_account.Credentials.from_service_account_info(key_dict)
+CNBC = get_CNBC()
+REUTERS = get_REUTERS()
+BSSN_STD = get_BSNN_STD()
+
+if CNBC:
+    full_today_news.extend(CNBC)
+if REUTERS:
+    full_today_news.extend(REUTERS)
+if BSSN_STD:
+    full_today_news.extend(BSSN_STD)
+
+found_news = False
+
+hist_data = download_data_from_cloud(creds)
+
+last_date = hist_data['date'][-1]
+today_flag = False
+today_econ_val = 0
+
+if (last_date.date() - datetime.today().date()).days == 0:
+    today_flag = True
+    today_econ_val = hist_data['points'][-1]
+
+
+
+st.header("Economic News Classifier")
+
+st.caption("This app scrapes three news website (CNBC, Reuters, Business Standard) and uses a fine-tuned Bert model to classify the news.")
+st.caption("A news is either 1 (positive), 0 (neutral) or -1 (negative). Then it outputs the mean of all the news founded, as an indicator of today economic state.")
+st.caption("Also it saves on a cloud database the results of the day, plotting every time a graph showing the data history.")
+
+
+st.subheader("Scraping News")
+with st.form('Scraping News'):
+    n = len(full_today_news)
+    if n == 0:
+        st.warning("No news today!!")
+    elif n>0:
+        st.success("Found %d news today!!"%n)
+        found_news = True
+    
+    submitted = st.form_submit_button("Continue")
+
+if submitted:
+    if found_news:
+
+        inputs, masks = preprocessing_for_bert(full_today_news)
+
+        dataset = TensorDataset(inputs, masks)
+        sampler = SequentialSampler(dataset)
+        dataloader = DataLoader(dataset, sampler=sampler, batch_size=1)
+
+        with st.spinner("Loading model..."):
+            model = load_model('bert_classifier.pt')
+
+        probs = bert_predict(model, dataloader)
+        labels = [gen_labels(np.argmax(p)) for p in probs]
+        points = [give_points(np.argmax(p)) for p in probs]
+        classified_news = pd.DataFrame()
+        classified_news['news'] = full_today_news
+        classified_news['label'] = labels
+        classified_news['points'] = points
+        final_val = np.mean(points)
+
+        if today_flag:
+            if final_val == today_econ_val:
+                pass
+            else:
+                load_data_on_cloud(datetime.now(), final_val, creds)
+        else:
+            load_data_on_cloud(datetime.now(), final_val, creds)
+
+        st.subheader("The evaluation of today economic situation is:")
+        st.write(final_val)
+        st.subheader("Historical data:")
+        plot_data = pd.DataFrame(download_data_from_cloud(creds))
+        st.line_chart(plot_data, x='date', y='points')
+
+        st.subheader("and all the news found:")
+        st.dataframe(classified_news)
+
+
+    else:
+        st.info("No news, no predictions, try later ;) (still get the historical data)", icon="❗")
+        plot_data = pd.DataFrame(download_data_from_cloud(creds))
+        st.line_chart(plot_data, x='date', y='points')
+
+
+
+
---
+++ scraping.py
@@ -0,0 +1,156 @@
+import requests
+from bs4 import BeautifulSoup
+from termcolor import colored
+from datetime import datetime
+
+
+#CNBC
+def get_CNBC(today_only = True):
+
+    page = requests.get("https://www.cnbc.com/world-economy/")
+
+    soup = BeautifulSoup(page.content, 'html.parser')
+
+    news_div = soup.find_all('div', {'class':'Card-titleContainer'})
+    dates_div = soup.find_all('div', {'class':'Card-cardFooter'})
+
+    news_title = []
+    news_date = []
+
+    for text in news_div:
+        news_title.append(text.find_all('a')[0].text)
+    for date in dates_div:
+        news_date.append(date.find_all('span', {'class':'Card-time'})[0].text)
+
+    #print(colored("\n\n----------CNBC----------", 'red', attrs=['bold']))
+    today_news = []
+    older_news = {'news':[],'date':[]}
+    for i,date in enumerate(news_date):
+        #print("%d. %s %s"%(i+1,news_title[i],date), end='\t')
+        if 'hours' in date or 'min' in date or 'hour' in date:
+            #print(colored("today",'blue',attrs=['bold']))
+            today_news.append(news_title[i])
+        else:
+            nice_date = date[5:]
+            m = nice_date[:3]
+            y = nice_date[-5:]
+            d = nice_date[4:-7]
+            final_date = datetime.strptime(d+" "+m+y,"%d %b %Y").date()
+            if final_date == datetime.today().date():
+                #print(colored("today",'blue',attrs=['bold']))
+                today_news.append(news_title[i])
+            else:
+                older_news['news'].append(news_title[i])
+                older_news['date'].append(date)
+                #print(colored("not today",'green',attrs=['bold']))
+    
+    if today_only:
+        return today_news
+    else:
+        return today_news, older_news
+
+
+
+#REUTERS
+
+def get_REUTERS(today_only = True):
+
+    page = requests.get("https://www.reuters.com/news/archive/economicNews")
+
+    soup = BeautifulSoup(page.content, 'html.parser')
+
+    news_div = soup.find_all('div', {'class':'story-content'})
+    dates_div = soup.find_all('time', {'class':'article-time'})
+
+    news_title = []
+    news_date = []
+
+    for text in news_div:
+        news_title.append(text.find_all('a')[0].text.strip())
+    for date in dates_div:
+        news_date.append(date.find_all('span', {'class':'timestamp'})[0].text)
+
+    #print(colored("\n\n----------REUTERS----------", 'red', attrs=['bold']))
+    today_news = []
+    older_news = {'news':[],'date':[]}
+    for i,date in enumerate(news_date):
+        #print("%d. %s %s"%(i+1,news_title[i],date), end='\t')
+        if 'EDT' in date:
+            today_news.append(news_title[i])
+            #print(colored("today",'blue',attrs=['bold']))
+        else:
+            older_news['news'].append(news_title[i])
+            older_news['date'].append(date)
+            #print(colored("not today",'green',attrs=['bold']))
+    
+    if today_only:
+        return today_news
+    else:
+        return today_news, older_news
+
+'''
+#NYTIMES
+
+page = requests.get("https://www.nytimes.com/section/business/economy")
+
+soup = BeautifulSoup(page.content, 'html.parser')
+
+news_div = soup.find_all('div', {'class':'css-1l4spti'})
+dates_div = soup.find_all('div', {'class':'css-1cp3ece'})
+
+news_title = []
+news_date = []
+
+for text in news_div:
+    news_title.append(text.find_all('a')[0].text)
+for date in dates_div:
+    print(date.span)
+    #news_date.append(date.find_all('span')[0].text)
+
+print(colored("\n\n----------NY TIMES----------", 'red', attrs=['bold']))
+for i,date in enumerate(news_date):
+    print("%d. %s %s"%(i+1,news_title[i],date))
+'''
+
+#BUSINESS-STD
+
+def get_BSNN_STD(today_only = True):
+
+    page = requests.get("https://www.business-standard.com/category/international-news-economy-1160102.htm")
+
+    soup = BeautifulSoup(page.content, 'html.parser')
+
+    news_div = soup.find_all('div', {'class':'listing-panel'})
+
+
+    news_title = []
+    news_date = []
+
+    for text in news_div:
+        list_els = text.find_all('li')
+        #print(list_els)
+        for el in list_els:
+            #print(el)
+            news_date.append(el.find('p').text)
+            news_title.append(el.find("h2").text)
+
+    #print(colored("\n\n----------BUSINESS STANDARD----------", 'red', attrs=['bold']))
+
+    today_news = []
+    older_news = {'news':[],'date':[]}
+
+    for i,date in enumerate(news_date):
+        #print("%d. %s %s"%(i+1,news_title[i],date), end='\t')
+        final_date = datetime.strptime(date,"%B %d, %Y, %A").date()
+        if final_date == datetime.today().date():
+            today_news.append(news_title[i])
+            #print(colored("today",'blue',attrs=['bold']))
+        else:
+            older_news['news'].append(news_title[i])
+            older_news['date'].append(date)
+            #print(colored("not today",'green',attrs=['bold']))
+    
+    if today_only:
+        return today_news
+    else:
+        return today_news, older_news
